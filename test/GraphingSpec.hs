6e0d27ccba8efcade771d63e970a33e3b6c9fce0
Wesley Van Melle <van.melle.wes@gmail.com>
2021-10-05 10:16:19 -0700

    fmt fix

--- test/GraphingSpec.hs
+++ test/GraphingSpec.hs
@@ -12,7 +12,7 @@ spec = do
   describe "unfold" $ do
     it "should unfold deeply" $ do
       let graph :: Graphing Int
-          graph = unfold [10] (\x -> if x > 0 then [x -2] else []) id
+          graph = unfold [10] (\x -> if x > 0 then [x - 2] else []) id
 
       expectDirect [10] graph
       expectDeps [10, 8, 6, 4, 2, 0] graph
